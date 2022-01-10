e7a8bb7b7f67300e0c7b48c5f8a8c4398af911bf
Atul Anand <atulcleaver@gmail.com>
2022-01-11 01:48:53 +0530

    Added the pixel art code.

---
+++ main.py
@@ -0,0 +1,18 @@
+from PIL import Image
+
+
+def pixelate(input_file_path, pixel_size):
+    image = Image.open(input_file_path)
+    image = image.resize(
+        (image.size[0] // pixel_size, image.size[1] // pixel_size),
+        Image.NEAREST
+    )
+    image = image.resize(
+        (image.size[0] * pixel_size, image.size[1] * pixel_size),
+        Image.NEAREST
+    )
+
+    image.show()
+
+
+pixelate("panda.jpeg", 12)
